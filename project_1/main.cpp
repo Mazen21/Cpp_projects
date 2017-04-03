db3278e41087b3ffa0c5b5c35a86b45da442ba48
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-03 18:00:35 +0100

    templates

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -2,28 +2,23 @@
 #include <iostream>
 using namespace std;
 
-void odd (int x);
-void even (int x);
+template <class T>
+
+T sum (T a, T b)
+{
+	T result;
+	result = a+b;
+	return result;
+}
 
 int main()
 {
-	int i;
-	do{
-		cout << "Please, enter number (0 to exit): ";
-		cin >> i;
-		odd (i);
-	}while(i !=0);
+	int i=5, j=6, k;
+	double f=2.0, g=0.5, h;
+	
+	k=sum<int>(i,j);
+	h=sum<double>(f,g);
+	cout <<k<< '\n';
+	cout <<h<< '\n';
 	return 0;
-}
-
-void odd (int x)
-{
-	if ((x%2)!=0) cout << "It is odd .\n";
-	else even (x);
-}
-
-void even(int x)
-{
-	if ((x%2)==0) cout << "It is even .\n";
-	else odd (x);
 }
