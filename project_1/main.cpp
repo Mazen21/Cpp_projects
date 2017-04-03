f0faaa9c37055d3ad9dcc54fffd3bfd61590141a
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-03 16:30:57 +0100

    first change in statements branch

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -9,7 +9,7 @@ int main()
 	getline (cin, mystr);
 	cout << "what's your favorite team? ";
 	getline (cin, mystr);
-	cout << "I like " << mystr << " too! \n";
+	cout << "I don't like " << mystr << " too! \n";
 	
 	return 1;
 }
