ad472fe51fecd574be44f934704d0cfee0ff85ca
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-03 15:54:48 +0100

    initial commit

---
+++ project_1/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string mystring;
+	mystring = "This is the initial string content";
+	cout << mystring <<endl;
+	mystring = "this is a different strin gcontent";
+	cout <<mystring <<endl;
+	return 0;
+}
