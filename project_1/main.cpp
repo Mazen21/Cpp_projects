b134bd720dabaef506f26d21a9de97da8be9a04a
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-03 17:36:22 +0100

    function and references

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -2,11 +2,28 @@
 #include <iostream>
 using namespace std;
 
+void odd (int x);
+void even (int x);
+
 int main()
 {
-int i,n;
-	for ( n=0, i = 100; n!=i; ++n, --i ){
-		cout << n << " .\n";
-	}
-	return 1;
+	int i;
+	do{
+		cout << "Please, enter number (0 to exit): ";
+		cin >> i;
+		odd (i);
+	}while(i !=0);
+	return 0;
+}
+
+void odd (int x)
+{
+	if ((x%2)!=0) cout << "It is odd .\n";
+	else even (x);
+}
+
+void even(int x)
+{
+	if ((x%2)==0) cout << "It is even .\n";
+	else odd (x);
 }
