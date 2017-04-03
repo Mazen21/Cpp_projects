6938069ce473994cf22ac5857b65d9daa6ed5ccc
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-03 17:22:46 +0100

    for loop

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 int main()
 {
-	string mystr;
-	cout << "What's your name? ";
-	getline (cin, mystr);
-	cout << "what's your favorite team? ";
-	getline (cin, mystr);
-	cout << "I don't like " << mystr << " too! \n";
-	
+int i,n;
+	for ( n=0, i = 100; n!=i; ++n, --i ){
+		cout << n << " .\n";
+	}
 	return 1;
 }
