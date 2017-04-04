3d6e788bc03b7470bf19e00c45c5726964e059d7
Mazen Aounallah <aounallah.mazen@gmail.com>
2017-04-04 17:58:42 +0100

    last commit
    Signed-off-by: Mazen Aounallah <aounallah.mazen@gmail.com>

--- Logger_cpp/main.cpp
+++ Logger_cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -44,12 +45,38 @@ void LoggerConsol::printV(string const &mystr){
 	cout <<"\033[1;35m"<<"VERBOSITY :"<< mystr<<"\033[0m\n";	
 }
 
+/*LoggerFile class*/
+
+class LoggerFile : Logger {
+  private:
+	ofstream myfile;
+  public:
+	void create_file();
+    void printE(string const &);
+	void printW(string const &);
+	void printV(string const &); 
+};
+
+void LoggerFile::create_file(){
+	
+	ofstream myfile("logfile.txt");
+}
+
+void LoggerFile::printE(string const &mystr){
+	//~ if(this->logfil.is_open()){
+	  //~ this->logfil<<"ERROR :"<< mystr;
+	//~ }
+}
+void LoggerFile::printW(string const &mystr){
+	cout <<"\033[1;33m"<<"WARNING :"<< mystr<<"\033[0m\n";	
+}
+void LoggerFile::printV(string const &mystr){
+	cout <<"\033[1;35m"<<"VERBOSITY :"<< mystr<<"\033[0m\n";	
+}
 
 
 int main(){
-	LoggerConsol Lc;
-	l.printE("error message");	
-	l.printW("warning message");
-	l.printV("verbosity message");
+	LoggerFile lc;
+	lc.create_file();
 	return 0;
 }
